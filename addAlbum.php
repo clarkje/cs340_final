d1c577b138125016dbb88a843ead97b38e2900b6
Jeromie Clark <clarkje@oregonstate.edu>
2016-05-24 00:39:24 -0700

    Added Style
    This looks a little better.

--- addAlbum.php
+++ addAlbum.php
@@ -11,10 +11,10 @@ if($mysqli->connect_errno){
 switch($_POST['action']) {
   case "addAlbum":
 
-
   break;
 }
 
+$success_addAlbum = true;
 
 ?>
 
@@ -23,12 +23,10 @@ switch($_POST['action']) {
   <meta author="Jeromie Clark, Andrew Kroes">
   <title>Add Albums and Tracks</title>
   <link rel="stylesheet" href="js/jquery/jquery-ui.min.css">
+	<link href="js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <script src="js/jquery/jquery-1.12.3.min.js"></script>
   <script src="js/jquery/jquery-ui.min.js"></script>
-	<!--
-	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-  -->
+	<script src="js/bootstrap/js/bootstrap.min.js"></script>
 
   <script>
   $(function() {
@@ -55,37 +53,78 @@ switch($_POST['action']) {
 					return false;
         }
       });
-
     });
   </script>
 </head>
 
 <body>
-  <fieldset>
-    <form method="post" action="">
+		<div class="container">
+		  <fieldset>
 
-      <input type="hidden" name="action" value="addAlbum">
+				<?php
+					if ($success_addAlbum) {
+						echo "<div class='alert alert-success' role='alert'>Album Added Successfully</div>";
+					} else if ($fail_addAlbum) {
+						echo "<div class='alert alert-danger' role='alert'>Album Add Failed</div>";
+					}
+				?>
+				<legend>Add Album</legend>
+		    <form method="post" action="">
+		      <input type="hidden" name="action" value="addAlbum">
+					<div class="form-group">
+						<label for="album_name">Album Name:</label>
+			      <input type="text" class="form-control" id="album_name">
+					</div>
+		      <p>
+						<div class="form-group">
+			        <label for="composer">Composer: </label>
+			        <input name="composer" class="form-control" value="" id="composer" placeholder="Composer Name">
+						</div>
+						<input type="hidden" name="composer_id" value="" id="composer_id">
+		      </p>
+		      <p>
+						<div class="form-group">
+			        <label for="artist">Artist: </label>
+			        <input name="artist" class="form-control" value="" id="artist" placeholder="Artist Name">
+					</div>
+					<input type="hidden" name="artist_id" value="" id="artist_id">
+		      </p>
+					<p>
+						<div class="form-group">
+			        <label for="genre">Genre: </label>
+							<select class="form-control" name="genre">
 
-			<label for="album_name">Album Name:</label>
-      <input type="text" id="album_name">
-      <p>
-        <label for="composer">Composer: </label>
-        <input name="composer" value="" id="composer">
-				<input type="hidden" name="composer_id" value="" id="composer_id">
-      </p>
-      <p>
-        <label for="artist">Artist: </label>
-        <input name="artist" value="" id="artist">
-				<input type="hidden" name="artist_id" value="" id="artist_id">
-      </p>
-    </form>
-  </fieldset>
+							<?php
+								// Show the available genres in a dropdown
 
+								$query = "SELECT genre_id, description FROM genre";
+								if(!($stmt = $mysqli->prepare($query))){
+									echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+								}
 
- <div class="ui-widget" style="margin-top:2em; font-family:Arial">
-   Result:
-   <div id="log" style="height: 200px; width: 300px; overflow: auto;" class="ui-widget-content"></div>
- </div>
+								$stmt->execute();
+								$stmt->bind_result($genre_id, $description);
 
+								while($stmt->fetch()) {
+									echo "<option value='".$genre_id."'>".$description."</option>";
+								}
+							?>
 
+							</select>
+					</div>
+						<input type="hidden" name="genre_id" value="" id="genre_id">
+		      </p>
+					<p>
+						<div class="form-group">
+			        <label for="release_date">Release Year:</label>
+			        <input type="number" max=2100 name="release_date" class="form-control" value="" id="release_date" maxlength=4 numeric>
+				      <label for="total_tracks">Total Tracks:</label>
+			        <input type="number" max=99 name="total_tracks" class="form-control" value="" id="total_tracks" maxlength=4>
+						</div>
+						<input type="hidden" name="total_tracks" value="" id="total_tracks" size="3">
+		      </p>
+					  <button type="button" class="btn btn-primary btn-lg">Add Album</button>
+		    </form>
+		  </fieldset>
+		</div>
 </body>
